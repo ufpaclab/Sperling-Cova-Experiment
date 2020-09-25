806b1987a5d2db0d280d119b9974866bfc1a6f9c
apfahler <70545166+apfahler@users.noreply.github.com>
2020-09-25 00:59:01 -0400

    Autocomplete off jspsych-survey-text

--- jspsych/plugins/jspsych-survey-text.js
+++ jspsych/plugins/jspsych-survey-text.js
@@ -100,7 +100,7 @@ jsPsych.plugins['survey-text'] = (function() {
       html += '<div id="jspsych-survey-text-preamble" class="jspsych-survey-text-preamble">'+trial.preamble+'</div>';
     }
     // start form
-    html += '<form id="jspsych-survey-text-form">'
+    html += '<form autocomplete="off" id="jspsych-survey-text-form">'
 
     // generate question order
     var question_order = [];
